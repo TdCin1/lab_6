d44e0e15dab4a57190e2245bf3d9f618e8695209
landon <lagic@ufl.edu>
2023-10-24 17:58:25 -0400

    added main loop

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ def encode_password(password):
             temp = int(temp) - 10
         new_password += str(temp)
     return new_password
+
+def main():
+    while True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        choice = input("Please enter an option: ")
+        if choice == 1:
+            password = input("Please enter your password to encode: ")
+            print("Your password has been encoded and stored!")
+            encoded_password = encode_password(password)
+
+        if choice == 2:
+            pass
+
+        if choice == 3:
+            exit()
+
+
